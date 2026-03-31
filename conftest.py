bd9bfce77970940b00f35947f3b79d6d1eff9c1e
vozhatyi-ux <vozhatyi@gmail.com>
2026-04-01 02:16:21 +0300

    добавил Faker

---
+++ conftest.py
@@ -0,0 +1,44 @@
+from core.clients.api_client import APIClient
+import pytest
+from datetime import datetime, timedelta
+from faker import Faker
+
+
+@pytest.fixture(scope="session")
+def api_client():
+    client = APIClient()
+    client.auth()
+    return client
+
+@pytest.fixture
+def booking_dates():
+    today = datetime.today()
+    checkin_date = today + timedelta(days=10)
+    checkout_date = checkin_date + timedelta(days=5)
+
+    return {
+        "checkin": checkin_date.strftime('%Y-%m-%d'),
+        "checkout": checkout_date.strftime('%Y-%m-%d')
+    }
+
+
+
+@pytest.fixture()
+def generate_random_booking_data(booking_dates):
+    faker = Faker()
+    firstname = faker.first_name()
+    lastname = faker.last_name()
+    totalprice = faker.random_number(digits=3)
+    depositpaid = faker.boolean()
+    additionalneeds = faker.sentence()
+
+    data = {
+        "firstname": firstname,
+        "lastname": lastname,
+        "totalprice": totalprice,
+        "depositpaid": depositpaid,
+        "bookingdates": booking_dates,
+        "additionalneeds": additionalneeds
+    }
+
+    return data
